b86f0804428ab3b343940419b5993ebd4ea7c6b6
Denis Belocerkovec <belocerkovecden@gmail.com>
2021-10-24 20:39:21 +0300

    first version

--- app/js/app.js
+++ app/js/app.js
@@ -1,29 +1,25 @@
 window.addEventListener('load', () => {
     let label = document.querySelector('.label');
     let planet__container = document.querySelector('.planet__container');
-    let dobIt = 0;
     let xOld = 0;
-    let positionElement = -150
+    let positionElement = -150;
     planet__container.addEventListener('mousemove', (e) => {
-        let xNew = e.screenX
-        clearTimeout(dobIt);
-        dobIt = setTimeout(() => {
-            if (xNew > xOld) {
-                positionElement += 3
-                if (positionElement >= 279) {
-                    positionElement = 279
-                } else {
-                    label.style.left = `-${positionElement}px`
-                }
+        let xNew = e.screenX;
+        if (xNew > xOld) {
+            positionElement += 3;
+            if (positionElement >= 279) {
+                positionElement = 279;
             } else {
-                positionElement -= 3
-                if (positionElement <= 30) {
-                    positionElement = 30
-                } else {
-                    label.style.left = `-${positionElement}px`
-                }
+                label.style.left = `-${positionElement}px`;
             }
-            xOld = e.screenX
-        }, 30)
+        } else {
+            positionElement -= 3
+            if (positionElement <= 30) {
+                positionElement = 30;
+            } else {
+                label.style.left = `-${positionElement}px`;
+            }
+        }
+        xOld = e.screenX;
     });
 });
