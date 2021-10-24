6acc34c0dd9d2935b552019cf8fda0f9307927bc
Denis Belocerkovec <belocerkovecden@gmail.com>
2021-10-24 21:24:54 +0300

    version

--- app/js/app.js
+++ app/js/app.js
@@ -3,17 +3,21 @@ window.addEventListener('load', () => {
     let planet__container = document.querySelector('.planet__container');
     let xOld = 0;
     let positionElement = -150;
-    planet__container.addEventListener('mousemove', (e) => {
+    planet__container.addEventListener('mousemove', moveLabel);
+    planet__container.addEventListener('click', moveLabel);
+    planet__container.addEventListener('touch', moveLabel);
+
+    function moveLabel (e) {
         let xNew = e.screenX;
         if (xNew > xOld) {
-            positionElement += 3;
-            if (positionElement >= 279) {
-                positionElement = 279;
+            positionElement += 4;
+            if (positionElement >= 285) {
+                positionElement = 285;
             } else {
                 label.style.left = `-${positionElement}px`;
             }
         } else {
-            positionElement -= 3
+            positionElement -= 4
             if (positionElement <= 30) {
                 positionElement = 30;
             } else {
@@ -21,5 +25,5 @@ window.addEventListener('load', () => {
             }
         }
         xOld = e.screenX;
-    });
+    }
 });
